84eb0365db5d4218c3f9bd7754eef8f647f28458
Anurag Sharma <anurags2111@gmail.com>
2018-07-01 22:07:07 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,22 @@
 import pandas as pd
+import numpy as np
 
 data =pd.read_csv('train.csv')
 
+ID = data.pop('ID')
+
 col = list(data)
+nul_ = data.isnull().sum()
+ddd = data.dtypes
+
+target = data['target']
+
+Cor__ = []s
+for i in data.columns[1:]:
+    ind = i
+    Cor__.append(data['target'].corr(data[i]))
+
+
+#Cor__ = np.array(Cor__)
+#arr = Cor__[~np.isnan(Cor__)]
 
